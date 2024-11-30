7713c9e6dfe10042ba95db777e0e8d525edb8dc5
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2024-11-30 13:45:51 +0300

    добавил багфиксиы для урлов с айпи адресом

--- swagger_coverage_py/results_writers/base_schemas_manager.py
+++ swagger_coverage_py/results_writers/base_schemas_manager.py
@@ -128,7 +128,9 @@ class ApiDocsManagerBase:
         return self._get_other_request_params(params_key="headers", params_in="header")
 
     def __get_output_subdir(self):
-        return re.match(r"(^\w*)://(.*)", self._uri.host).group(2)
+        # TODO и тут поправил
+        return re.match(r"(^\w*)://(.*)", self._uri.host).group(2).replace(".", "_").replace(":", "_")
+
 
     def write_schema(self):
         schema_dict = self._get_schema()
@@ -137,6 +139,8 @@ class ApiDocsManagerBase:
             "/", "-"
         ).replace(":", "_")
         path_ = f"swagger-coverage-output/{self.__get_output_subdir()}"
+        # TODO: Здесь поправил
+        pathlib.Path(path_).mkdir(parents=True, exist_ok=True)
         file_path = f"{path_}/{file_name}".split("?")[0]
         file_path = f"{file_path} ({rnd}).{API_DOCS_FORMAT}"
 
